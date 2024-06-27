1e5ed884e31a15dcf4a07926cbcee66024652619
Wolfhart Feldmeier <wolfhart.feldmeier@uni-jena.de>
2024-06-27 14:18:32 +0200

    also birth-death involution

--- uniform-split-merge.jl
+++ uniform-split-merge.jl
@@ -8,8 +8,11 @@ struct Parameters{T}
 end
 
 Parameters(n::Int) = Parameters([2 * @SVector(rand(3)) .- 1 for _ in 1:n])
+struct ParameterDist{D<:DiscreteUnivariateDistribution}
+    n_points_dist::D
+end
 
-function logdensity(n_points_dist, params::Parameters)
+function Distributions.logpdf(d::ParameterDist, params::Parameters)
     points = params.points
     n_points = length(points)
 
@@ -22,53 +25,58 @@ function logdensity(n_points_dist, params::Parameters)
     else
         # independent uniform distribution in 2x2x2 cube
         logpdf_points = -log(8) * n_points
-        return logpdf_points + logpdf(n_points_dist, n_points)
+        return logpdf_points + logpdf(d.n_points_dist, n_points)
     end
 end
 
-function split_merge_involution_weighted(points, aux)
-    if aux.split
-        original_point = points[aux.point_split_idx]
-        T = eltype(original_point)
-        point1 = original_point + aux.point_delta
-        point2 = original_point - aux.point_delta
-        points[aux.point_split_idx] = point1
-        points = insert!(points, aux.insert_idx, point2)
-        point_merge_idx = if aux.insert_idx <= aux.point_split_idx
-            aux.point_split_idx + 1
-        else
-            aux.point_split_idx
-        end
+function involutive_mc_step(
+    params::Parameters{T}, param_dist::ParameterDist, involution, make_aux_dist
+) where {T<:Real}
+    log_p_old = logpdf(param_dist::ParameterDist, params)
+    aux_dist_old = make_aux_dist(params)
+    aux_old = rand(aux_dist_old)
+    log_q_old = logpdf(aux_dist_old, aux_old)
 
-        lad = log(T(8))  # log abs det of Jacobian
+    params, aux_new, lad_jacobian = involution(params, aux_old)
 
-        return points, (; split=false, point_merge_idx, point_idx2=aux.insert_idx), lad
+    log_p_new = logpdf(param_dist::ParameterDist, params)
+    aux_dist_new = make_aux_dist(params)
+    log_q_new = logpdf(aux_dist_new, aux_new)
+
+    # println("Target acceptance ratio: $(exp(log_p_new - log_p_old))")
+
+    acceptance_probability = exp(
+        log_p_new + log_q_new - log_p_old - log_q_old + lad_jacobian
+    )
+
+    println("$(aux_old) q acc ratio: $(exp(log_q_new - log_q_old)), p acc ratio: $(exp(log_p_new - log_p_old)), J: $(exp(lad_jacobian)), a: $acceptance_probability")
+    # println("$(d[aux_old.split]) q acc ratio: $(exp(log_q_new - log_q_old)), p acc ratio: $(exp(log_p_new - log_p_old)), log_p_new: $log_p_new,  log_p_old: $log_p_old, J: $(exp(lad_jacobian))")
+
+    return if rand() < acceptance_probability
+        true, aux_old
     else
-        point1 = points[aux.point_merge_idx]
-        point2 = points[aux.point_idx2]
-        T = eltype(point1)
-        point_delta = T(0.5) * (point1 - point2)
-        point = point2 + point_delta
-        points[aux.point_merge_idx] = point
-        deleteat!(points, aux.point_idx2)
-        point_split_idx = if aux.point_idx2 < aux.point_merge_idx
-            aux.point_merge_idx - 1
-        else
-            aux.point_merge_idx
-        end
-
-        lad = -log(T(8))  # log abs det of Jacobian
-
-        points,
-        (;
-            split=true, point_split_idx, insert_idx=aux.point_idx2, point_delta
-        ),
-        lad
+        params, _, _ = involution(params, aux_new)
+        false, aux_old
     end
 end
 
-function sample_aux_split_merge(state::Parameters{T}) where {T}
-    points = state.points
+function sample(params, param_dist, involution, make_aux_dist, n)
+    accepted = Bool[]
+    n_points = Int[]
+    for i in 1:n
+        a, _ = involutive_mc_step(params, param_dist, involution, make_aux_dist)
+        push!(accepted, a)
+        push!(n_points, length(params.points))
+    end
+    return accepted, n_points
+end
+
+struct SplitMergeAuxDist{T}
+    state::Parameters{T}
+end
+
+function Base.rand(d::SplitMergeAuxDist{T}) where T
+    points = d.state.points
     n_points = length(points)
     bandwidth = T(0.2)
     split = n_points < 2 ? true : rand(Bernoulli(T(0.5)))
@@ -84,14 +92,14 @@ function sample_aux_split_merge(state::Parameters{T}) where {T}
         # w ./= sum(w)
         # point_merge_idx = rand(Distributions.Categorical(w))
         point_merge_idx = rand(DiscreteUniform(1, n_points))
-        w = distance_weights(state.points, point_merge_idx, bandwidth)
+        w = distance_weights(points, point_merge_idx, bandwidth)
         point_idx2 = rand(Distributions.Categorical(w))
         return (; split, point_merge_idx, point_idx2)
     end
 end
 
-function logpdf_aux_split_merge(aux, state::Parameters{T}) where {T}
-    points = state.points
+function Distributions.logpdf(d::SplitMergeAuxDist{T}, aux) where {T}
+    points = d.state.points
     n_points = length(points)
     bandwidth = T(0.2)
     score = n_points < 2 ? zero(T) : logpdf(Bernoulli(T(0.5)), aux.split)
@@ -106,7 +114,7 @@ function logpdf_aux_split_merge(aux, state::Parameters{T}) where {T}
         # w ./= sum(w)
         # score += logpdf(Distributions.Categorical(w), aux.point_merge_idx)
         score += logpdf(DiscreteUniform(1, n_points), aux.point_merge_idx)
-        w = distance_weights(state.points, aux.point_merge_idx, bandwidth)
+        w = distance_weights(points, aux.point_merge_idx, bandwidth)
         score += logpdf(Distributions.Categorical(w), aux.point_idx2)
     end
     score
@@ -129,60 +137,91 @@ function distance_weights(points::AbstractVector{<:AbstractVector{T}}, ref_idx, 
     end
 end
 
-function sample_split_merge(
-    params::Parameters{T}, n_points_prior_dist
-) where {T<:Real}
-    log_p_old = logdensity(n_points_prior_dist, params)
-    aux_old = sample_aux_split_merge(params)
-    log_q_old = logpdf_aux_split_merge(aux_old, params)
+function split_merge_involution_weighted(state::Parameters, aux)
+    points = state.points
+    if aux.split
+        original_point = points[aux.point_split_idx]
+        T = eltype(original_point)
+        point1 = original_point + aux.point_delta
+        point2 = original_point - aux.point_delta
+        points[aux.point_split_idx] = point1
+        insert!(points, aux.insert_idx, point2)
+        point_merge_idx = if aux.insert_idx <= aux.point_split_idx
+            aux.point_split_idx + 1
+        else
+            aux.point_split_idx
+        end
 
-    _, aux_new, lad_jacobian = split_merge_involution_weighted(params.points, aux_old)
+        lad = log(T(8))  # log abs det of Jacobian
 
-    log_p_new = logdensity(n_points_prior_dist, params)
-    log_q_new = logpdf_aux_split_merge(aux_new, params)
-
-    # println("Target acceptance ratio: $(exp(log_p_new - log_p_old))")
-
-    acceptance_probability = exp(
-        log_p_new + log_q_new - log_p_old - log_q_old + lad_jacobian
-    )
-    d = Dict(true => "Split", false => "Merge")
-
-    println("$(d[aux_old.split]) q acc ratio: $(exp(log_q_new - log_q_old)), p acc ratio: $(exp(log_p_new - log_p_old)), J: $(exp(lad_jacobian)), a: $acceptance_probability")
-    # println("$(d[aux_old.split]) q acc ratio: $(exp(log_q_new - log_q_old)), p acc ratio: $(exp(log_p_new - log_p_old)), log_p_new: $log_p_new,  log_p_old: $log_p_old, J: $(exp(lad_jacobian))")
-
-    return if rand() < acceptance_probability
-        # accept
-        # if aux_old.split
-        #     point1 = params.points[aux_old.point_split_idx]
-        #     point2 = params.points[aux_old.insert_idx]
-        #     println("Splitted point at index $(aux_old.point_split_idx) into $point1, $point2. P_acc = $acceptance_probability")
-        # else
-        #     point = params.points[aux_old.point_merge_idx]
-        #     println("Merged point at index $(aux_old.point_merge_idx) with point at index $(aux_old.point_idx2): $point. P_acc = $acceptance_probability")
-        # end
-        true, aux_old.split
+        return state, (; split=false, point_merge_idx, point_idx2=aux.insert_idx), lad
     else
-        # if aux_old.split
-        #     println("Rejected split of point at index $(aux_old.point_split_idx). P_acc = $acceptance_probability")
-        # else
-        #     println("Rejected merge of point at index $(aux_old.point_merge_idx) with point at index $(aux_old.point_idx2). P_acc = $acceptance_probability")
-        # end
-        # reject. Call involution again to go back to old state
-        split_merge_involution_weighted(params.points, aux_new)
-        false, aux_old.split
+        point1 = points[aux.point_merge_idx]
+        point2 = points[aux.point_idx2]
+        T = eltype(point1)
+        point_delta = T(0.5) * (point1 - point2)
+        point = point2 + point_delta
+        points[aux.point_merge_idx] = point
+        deleteat!(points, aux.point_idx2)
+        point_split_idx = if aux.point_idx2 < aux.point_merge_idx
+            aux.point_merge_idx - 1
+        else
+            aux.point_merge_idx
+        end
+
+        lad = -log(T(8))  # log abs det of Jacobian
+
+        state,
+        (;
+            split=true, point_split_idx, insert_idx=aux.point_idx2, point_delta
+        ),
+        lad
     end
 end
 
-function sample(params, n_points_prior_dist, n)
-    accepted = Bool[]
-    split = Bool[]
-    n_points = Int[]
-    for i in 1:n
-        a, s = sample_split_merge(params, n_points_prior_dist)
-        push!(accepted, a)
-        push!(split, s)
-        push!(n_points, length(params.points))
+struct BirthDeathAuxDist{T, D<:Distribution}
+    state::Parameters{T}
+    point_dist::D
+end
+
+function Base.rand(
+    d::BirthDeathAuxDist{T}
+) where {T}
+    points = d.state.points
+    n_points = length(points)
+    birth = n_points < 2 ? true : rand(Bernoulli(T(0.5)))
+    if birth
+        point_birth_idx = rand(DiscreteUniform(1, n_points + 1))
+        point = rand(d.point_dist)
+        return (; birth, point_birth_idx, point)
+    else
+        point_death_idx = rand(DiscreteUniform(1, n_points))
+        return (; birth, point_death_idx)
     end
-    return accepted, split, n_points
+end
+
+function Distributions.logpdf(d::BirthDeathAuxDist{T}, aux) where {T}
+    points = d.state.points
+    n_points = length(points)
+    score = n_points < 2 ? zero(T) : logpdf(Bernoulli(T(0.5)), aux.birth)
+    if aux.birth
+        score += logpdf(DiscreteUniform(1, n_points + 1), aux.point_birth_idx)
+        score += logpdf(d.point_dist, aux.point)
+    else
+        score += logpdf(DiscreteUniform(1, n_points), aux.point_death_idx)
+    end
+    score
+end
+
+function birth_death_involution(state::Parameters{T}, aux) where {T}
+    points = state.points
+    if aux.birth
+        i = aux.point_birth_idx
+        insert!(points, i, aux.point)
+        return state, (; birth=false, point_death_idx=i), zero(T)
+    else
+        i = aux.point_death_idx
+        point = popat!(points, i)
+        return state, (; birth=true, point_birth_idx=i, point), zero(T)
+    end
 end
